238b92ded3c79cc8ddcfb65ae7e2a514d47d1632
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-06-26 15:36:33 -0400

    Update init.cpp

--- prog/init.cpp
+++ prog/init.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 
 class Inv {
-        std::vector<char> n_cons;
-        std::vector<char> n_vowl;
+        std::vector<char> s_cons;
+        std::vector<char> s_vowl;
     public:
-        char r_cons(int p) { return n_cons[p]; }
-        char r_vowl(int p) { return n_vowl[p]; }
+        char r_cons(int p) { return s_cons[p]; }
+        char r_vowl(int p) { return s_vowl[p]; }
         
-        void add_cons(char n) { n_cons.push_back(n); }
-        void add_vowl(char n) { n_vowl.push_back(n); }
-        void del_cons(int p) { n_cons.erase(n_cons.begin() + (p - 1)); }
-        void del_vowl(int p) { n_vowl.erase(n_vowl.begin() + (p - 1)); }
+        void add_cons(char n) { s_cons.push_back(n); }
+        void add_vowl(char n) { s_vowl.push_back(n); }
+        void del_cons(int p) { s_cons.erase(s_cons.begin() + (p - 1)); }
+        void del_vowl(int p) { s_vowl.erase(s_vowl.begin() + (p - 1)); }
 };
