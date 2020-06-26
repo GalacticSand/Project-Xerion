561c5590977334bcdaae2fa04ca96eec290b5c80
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-06-26 15:34:52 -0400

    Update init.cpp

--- prog/init.cpp
+++ prog/init.cpp
@@ -7,8 +7,9 @@ class Inv {
     public:
         char r_cons(int p) { return n_cons[p]; }
         char r_vowl(int p) { return n_vowl[p]; }
-        void add_cons(char nc) { n_cons.push_back(nc); }
+        
+        void add_cons(char n) { n_cons.push_back(n); }
+        void add_vowl(char n) { n_vowl.push_back(n); }
         void del_cons(int p) { n_cons.erase(n_cons.begin() + (p - 1)); }
-        void add_vowl(char nv) { n_vowl.push_back(nv); }
         void del_vowl(int p) { n_vowl.erase(n_vowl.begin() + (p - 1)); }
 };
