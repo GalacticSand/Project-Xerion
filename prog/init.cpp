fc01bbb18944dd1ca372a80506543502a7e9d0a4
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-06-26 19:57:39 -0400

    Update init.cpp

--- prog/init.cpp
+++ prog/init.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 
+// Class Definitions
 class langInv {
         std::vector<std::string> w_co;
         std::vector<std::string> w_vo;
@@ -9,8 +10,22 @@ class langInv {
         std::string r_co(int p) { return w_co[p]; }
         std::string r_vo(int p) { return w_vo[p]; }
         
+        int r_co_size() { return w_co.size(); }
+        int r_vo_size() { return w_vo.size(); }
+        
         void add_co(std::string n) { w_co.push_back(n); }
         void add_vo(std::string n) { w_vo.push_back(n); }
         void del_co(int p) { w_co.erase(w_co.begin() + (p - 1)); }
         void del_vo(int p) { w_vo.erase(w_vo.begin() + (p - 1)); }
+        
+        void change_co_bnum(int p, std::string n) {
+                w_co[p] = n;}
+        void change_co_blet(std::string o, std::string n) {
+                int i = 0;
+                while (o != w_co[i]) {
+                        i++;}
+                change_co_bnum(i, n);}
+                
 };
+
+// Function Definitions
