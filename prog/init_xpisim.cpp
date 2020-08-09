6a5a748360cf719ea25895ba522d0c2f63cd3be0
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-08-08 20:08:01 -0400

    Update init_xpisim.cpp

--- prog/init_xpisim.cpp
+++ prog/init_xpisim.cpp
@@ -1,15 +1,25 @@
 #include <string>
 #include <vector>
 
-// Function Definitions
+// Variables and Arrays
+std::vector<std::string> sh_init, sh_res, sh_con;
+
+// Basic Functions
 template <class dx_type>
 int get_index(std::vector<dx_type> list, dx_type item) {
     int v;
-    for (int i = 0; i < list.size(); i++) { if (list[i] = item) { v = i; } }
+    for (int i = 0; i < list.size(); i++) { if (list[i] == item) { v = i; } }
     return v;
 }
 
-// Class Definitions
+// Advanced Functions
+void phon_sh_rule(std::string init, std::string res, std::string con) {
+    sh_init.push_back(init);
+    sh_res.push_back(res);
+    sh_con.push_back(con);
+}
+
+// Classes
 class phon_type { 
         std::string tag;
         std::string col_stag;
@@ -18,17 +28,15 @@ class phon_type {
         phon_type(std::string t) { tag = t; }
         void ch_phon(int dx, std::string n_phon) { col[dx] = n_phon; }
         void ch_spec_phon(std::string o_phon, std::string n_phon) { col[get_index<std::string>(col, o_phon)] = n_phon; }
-        }
 };
 
-class phon_sh_rule { 
-        std::string init;
-        std::string res;
-        std::string con;
+class word_list {
+        std::vector<std::string> list;
+        std::vector<std::string> wdec;
     public:
-        phon_sh_rule(std::string in, std::string r, std::string c) {
-            init = in;
-            res = r;
-            con = c;
+        void split_word(int dx) {
+            wdec = {};
+            std::string w = list[dx];
+            for (int i = 0; i < w.length(); i++) { wdec.push_back(std::to_string(w.at(i))); }
         }
 };
