6cc5137c2d8d9d928dbdb868a9724440f6a18ec8
GalacticSand <59291313+GalacticSand@users.noreply.github.com>
2020-08-08 21:44:48 -0400

    Update init_xpisim.cpp

--- prog/init_xpisim.cpp
+++ prog/init_xpisim.cpp
@@ -22,10 +22,13 @@ void phon_sh_rule(std::string init, std::string res, std::string con) {
 // Classes
 class phon_type { 
         std::string tag;
-        std::string col_stag;
+        std::vector<std::string> stag;
         std::vector<std::string> col;
     public:
         phon_type(std::string t) { tag = t; }
+        std::string get_tag() { return tag; }
+        std::string get_stag(int dx) { return stag[dx]; }
+        std::string get_phon(int dx) { return col[dx]; }
         void ch_phon(int dx, std::string n_phon) { col[dx] = n_phon; }
         void ch_spec_phon(std::string o_phon, std::string n_phon) { col[get_index<std::string>(col, o_phon)] = n_phon; }
 };
@@ -34,6 +37,10 @@ class word_list {
         std::vector<std::string> list;
         std::vector<std::string> wdec;
     public:
+        std::string get_word(int dx) { return list[dx]; }
+        std::string get_word_lttr(int dx, int lttr) { return std::to_string(list[dx].at(lttr)); }
+        std::string get_split_lttr(int dx) { return wdec[dx]; }
+        void sh_split_lttr(int dx, std::string n_lttr) { wdec[dx] = n_lttr; }
         void split_word(int dx) {
             wdec = {};
             std::string w = list[dx];
